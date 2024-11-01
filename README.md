29d614eb7dda47bec4bc35de265a5f1f04f1058c
Mrunmayeed <mrunmayee.dhapre.98@gmail.com>
2024-10-31 21:01:54 -0700

    basic app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ElectronicVoting
 CS218 Midterm
+
+pip install -r requirements.txt
+
+export FLASK_APP=app.py
+export FLASK_ENV=development
+flask run
---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+USERS = {
+    "admin@sjsu.edu": {"username": "admin", "password": "admin123", "role": "admin"},
+    "juan@sjsu.edu": {"username": "judge1", "password": "judge001", "role": "judge"},
+    "j2@sjsu.edu": {"username": "judge2", "password": "judge002", "role": "judge"},
+    "j3@sjsu.edu": {"username": "judge3", "password": "judge003", "role": "judge"}
+}
+
+JUDGES = ["judge1", "judge2", "judge3"]
